1120789c923da5944544579c390f2bc111a16956
Michael Jordan <mjorda39@uncc.edu>
2018-04-04 17:32:56 +0000

    Generate and run Characters migration

---
+++ db/migrate/20180404173223_create_characters.rb
@@ -0,0 +1,13 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :gender
+      t.string :race
+      t.string :charclass
+      t.text :story
+
+      t.timestamps
+    end
+  end
+end
