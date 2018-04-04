8141b27be2c9a878fa46224e998122c62adb04fc
Michael Jordan <mjorda39@uncc.edu>
2018-04-04 18:12:44 +0000

    Add create and show functionality

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,10 +1,25 @@
 class CharactersController < ApplicationController
     def index
+        @characters = Character.all
     end
     
     def new
+        @character = Character.new
     end
     
     def create
+        @character = Character.new(character_params)
+        
+        @character.save
+        redirect_to @character
+    end
+    
+    def show
+        @character = Character.find(params[:id])
     end
 end
+
+private
+    def character_params
+        params.require(:character).permit(:name, :gender, :race, :charclass, :story)
+    end
