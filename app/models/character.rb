f2e7455ef6e346aa31b5267d3ddd7f458fcdc465
Michael Jordan <mjorda39@uncc.edu>
2018-04-18 20:36:05 +0000

    Add validation for character attributes

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ApplicationRecord
     has_many :abilities, dependent: :destroy
+    validates :name, presence: true, length: { minimum: 5, maximum: 15 }
+    validates :gender, presence: true, length: { minimum: 1, maximum: 10 }
+    validates :race, presence: true, length: { minimum: 5, maximum: 15 }
+    validates :charclass, presence: true, length: { minimum: 5, maximum: 15 }
+    validates :story, presence: true, length: { minimum: 10, maximum: 100 }
 end
